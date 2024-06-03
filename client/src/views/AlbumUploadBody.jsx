b0cc419b8e7d776a2975447dae6f261a777d086e
KaReeeeeeeEM <s2kizzyjunior13052002@gmail.com>
2024-06-03 17:44:38 +0300

    changed the words on the album section button

--- client/src/views/AlbumUploadBody.jsx
+++ client/src/views/AlbumUploadBody.jsx
@@ -9,7 +9,7 @@ const AlbumUploadBody = ({back}) => {
         <div className='text-gray-400 w-screen px-4 z-10 h-20 flex justify-between items-center text-lg bg-white fixed left-2 top-20 lg:top-16 lg:left-0'>
             <span><a href={back}><span>Dashboard</span></a>/ <span className='text-purple-600'>Albums</span></span>
             <a className='mr-8 lg:mr-24 px-2 py-1 text-md lg:px-4 lg:py-2.5 bg-purple-600 rounded-lg text-white font-bold cursor-pointer'>
-              Create Song
+              Create Album
             </a>
         </div>
         <div className='w-full lg:w-screen h-[720px] lg:h-full absolute left-0 mt-12 transition ease-in-out duration-500 hover:opacity-80 overflow-auto'>
