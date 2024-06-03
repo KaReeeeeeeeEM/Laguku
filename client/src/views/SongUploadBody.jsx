2f14fae1f045610cd3dc4b8ad108545a6561518a
KaReeeeeeeEM <s2kizzyjunior13052002@gmail.com>
2024-06-03 14:33:23 +0300

    fixed the spacing problem for the action bar of small devices

--- client/src/views/SongUploadBody.jsx
+++ client/src/views/SongUploadBody.jsx
@@ -4,10 +4,13 @@ import SongUploadTable from './SongUploadTable'
 const SongUploadBody = ({back}) => {
   return (
     <div className='mt-28 px-6 lg:px-12 flex flex-col items-start'>
-        <div className='text-gray-400 w-screen z-50 h-12 text-lg bg-white fixed left-6 top-16 lg:top-24 lg:left-12'>
-            <a href={back}><span>Dashboard</span></a>/ <span className='text-purple-600'>Songs</span>
+        <div className='text-gray-400 w-screen z-10 h-12 flex justify-between items-center text-lg bg-white fixed left-2 top-20 lg:top-24 lg:left-12'>
+            <span><a href={back}><span>Dashboard</span></a>/ <span className='text-purple-600'>Songs</span></span>
+            <a className='mr-8 lg:mr-24 px-3 py-1 text-md lg:px-4 lg:py-2 bg-purple-600 rounded-lg text-white font-bold cursor-pointer'>
+              Create Song
+            </a>
         </div>
-        <div className='w-full lg:w-screen h-[720px] absolute left-0 mt-12 overflow-auto'>
+        <div className='w-full lg:w-screen h-[720px] absolute left-0 mt-12 transition ease-in-out duration-500 hover:opacity-80 overflow-auto'>
             <SongUploadTable />
         </div>
     </div>
